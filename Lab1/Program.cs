1ff2ac8b7318094f0c25d9a6ac10a456d781cf4d
LefterStud <lefter.school@gmail.com>
2023-09-12 20:53:21 +0300

    Прибрані зайві дужки, виправлені назви параметрів

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -7,7 +7,7 @@ namespace Lab1
         static void Main(string[] args)
         {
             Random r = new Random();
-            double number1 = (r.NextDouble() * 21) - 10, number2 = (r.NextDouble() * 21) - 10;
+            double number1 = r.NextDouble() * 21 - 10, number2 = r.NextDouble() * 21 - 10;
 
             Console.WriteLine("({0} * {1})^2 = {2}", number1, number2, Squaring(number1, number2));
         }
@@ -15,13 +15,12 @@ namespace Lab1
         /// <summary>
         /// Функція зведення у квадрат добутку двох дійсних чисел
         /// </summary>
-        /// <param name="number1">множник</param>
-        /// <param name="number2">множник</param>
-        /// <returns>Квадрат добутку</returns>
+        /// <param name="number1">множник1</param>
+        /// <param name="number2">множник2</param>
+        /// <returns>Квадрат добутку двох дійсних чисел</returns>
         static double Squaring(double number1, double number2)
         {
             return Math.Pow(number1 * number2, 2);
         }
-
     }
 }
