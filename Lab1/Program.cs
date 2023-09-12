c6374a3aa52e9e05a97d2c3704993e2e5af098ab
LefterStud <lefter.school@gmail.com>
2023-09-12 20:30:40 +0300

    Задача вирішена

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Lab1
+﻿//Task: 16. Реалізувати функцію зведення у квадрат доутку двох дійсних чисел
+namespace Lab1
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random r = new Random();
+            double number1 = (r.NextDouble() * 21) - 10, number2 = (r.NextDouble() * 21) - 10;
+
+            Console.WriteLine("({0} * {1})^2 = {2}", number1, number2, Squaring(number1, number2));
         }
+
+        /// <summary>
+        /// Функція зведення у квадрат добутку двох дійсних чисел
+        /// </summary>
+        /// <param name="number1">множник</param>
+        /// <param name="number2">множник</param>
+        /// <returns>Квадрат добутку</returns>
+        static double Squaring(double number1, double number2)
+        {
+            return Math.Pow(number1 * number2, 2);
+        }
+
     }
 }
